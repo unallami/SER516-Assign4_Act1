fed7ab30b7783184dcfb02b27f60eff924af1a04
amehlhas <a.mehlhase@asu.edu>
2024-10-08 19:54:29 -0700

    New test class

---
+++ src/test/java/edu/asu/amexgi/rest/grocery/controllers/GroceryControllerTest2.java
@@ -0,0 +1,37 @@
+package edu.asu.amexgi.rest.grocery.controllers;
+
+import edu.asu.amexgi.rest.grocery.services.GroceryServices;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class GroceryControllerTest2 {
+
+    @Autowired
+    MockMvc mvc;
+
+    @MockBean
+    GroceryServices groceryServices;
+
+    @Test
+    void getGroceryItemByIdTest() throws Exception {
+        mvc.perform(get("/api/groceries/MLK"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("{'item':'milk'}"))
+                .andExpect(content().json("{'id':'MLK'}"));
+    }
+
+    @Test
+    void getGroceryItemByIdExceptionTest() throws Exception {
+        mvc.perform(get("/api/groceries/MLK1")).andExpect(status().is5xxServerError());
+    }
+}
